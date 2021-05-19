f02452852f5545a1549806ba7fbb7c429399f2ec
SumiSeo <qkobr94@gmail.com>
2021-05-19 10:08:03 +0900

    added section display

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const section3 =document.querySelector(".main__third");
 const section2 =document.querySelector(".main__second");
 const nav = document.querySelector(".header__nav");
 const navBar = document.querySelector(".header__nav__box ");
-
+const thirdBoxes = document.querySelector(".third__boxes");
+const thirdBox = document.querySelectorAll(".third__box");
 
 btnCommunity.addEventListener("click", function(){
     section3.scrollIntoView({behavior:'smooth'});
@@ -42,7 +43,7 @@ observerMainSecond.observe(section2)
 
 nav.addEventListener("mouseover", function(e){
     const link = e.target;
-    console.log(e.target)
+    // console.log(e.target)
     const siblings = link.closest('.header__nav').querySelectorAll("h3")
     siblings.forEach(function(el){
         if (el !== link) el.style.opacity = 0.4;
@@ -51,13 +52,66 @@ nav.addEventListener("mouseover", function(e){
 
 nav.addEventListener("mouseout", function(e){
     const link = e.target;
-    console.log(e.target)
+    // console.log(e.target)
     const siblings = link.closest('.header__nav').querySelectorAll("h3")
     siblings.forEach(function(el){
         if (el !== link) el.style.opacity = 1;
     })
 
 })
+
+
+//////////////////////////////////////////////////////////////////
+console.log(thirdBox)
+
+const displaySection3 = function(entries, observer){
+    const [entry] = entries;
+    console.log(entry);
+    if (!entry.isIntersecting) return;
+    else {
+    const allboxes =entry.target.closest(".third__boxes").querySelectorAll(".third__box")
+    allboxes.forEach(function(box){
+        box.classList.remove("active2")
+    })
+   
+}};
+
+const observerMainThird = new IntersectionObserver(displaySection3, {
+    root:null,
+    threshold:0,
+});
+
+thirdBox.forEach(function(box){
+    observerMainThird.observe(box);
+    console.log(box);
+});
+
+
+////const allSections = document.querySelectorAll(".section")
+
+// const revealSection = function(entries, observer){
+//     const [entry] = entries;
+//     if(!entry.isIntersecting) return;
+  
+//     entry.target.classList.remove("section--hidden");
+//     observer.unobserve(entry.target);
+  
+//   };
+  
+//   const sectionObserver = new IntersectionObserver(revealSection, {
+//     root:null,
+//     threshold:0.15,
+//   });
+  
+  
+//   allSections.forEach(function(section) {
+//     sectionObserver.observe(section);
+//     section.classList.add("section--hidden")
+//   })
+  
+
+
+
 
 // const handleHover = function(e,opacity){
  
@@ -80,3 +134,4 @@ nav.addEventListener("mouseout", function(e){
 //   nav.addEventListener("mouseout", function(e) {
 //     handleHover(e, 1)
 //   });
+

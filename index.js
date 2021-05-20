56ca833138a1ad09cbe13236c7bcdd334c578b1b
SumiSeo <qkobr94@gmail.com>
2021-05-20 23:52:33 +0900

    try to making slide

--- index.js
+++ index.js
@@ -2,12 +2,29 @@
 
 const btnCommunity = document.querySelector(".nav__box--3");
 const btnRetail = document.querySelector(".nav__box--2");
+const btnFood = document.querySelector(".nav__box--1")
 const section3 =document.querySelector(".main__third");
 const section2 =document.querySelector(".main__second");
+const section1 = document.querySelector(".main__first")
 const nav = document.querySelector(".header__nav");
-const navBar = document.querySelector(".header__nav__box ");
+const navBar = document.querySelectorAll(".header__nav__box");
 const thirdBoxes = document.querySelector(".third__boxes");
 const thirdBox = document.querySelectorAll(".third__box");
+const slider = document.querySelector(".main__slides");
+const slide = document.querySelectorAll(".main__slide");
+const btnRight = document.querySelector(".btn__right");
+
+
+btnFood.addEventListener("click", function(){
+    section1.scrollIntoView({behavior:'smooth'});
+})
+
+
+
+
+
+
+
 
 btnCommunity.addEventListener("click", function(){
     section3.scrollIntoView({behavior:'smooth'});
@@ -40,19 +57,45 @@ const observerMainSecond = new IntersectionObserver(displaySection2 , {
 
 observerMainSecond.observe(section2)
 
+// nav.addEventListener("mouseover", function(e){
+//     const link = e.target;
+//     console.log(link)
+//     const siblings = link.closest(".header__nav").querySelectorAll("h3");
+//     const final = link.querySelector("h3")
+//    siblings.forEach(function(s){
+//        if (s !== final){
+//        s.style.opacity = 0.4;}
+//    });
+
+// })
+
+
+
+
+
+
+
 
 nav.addEventListener("mouseover", function(e){
     const link = e.target;
     // console.log(e.target)
-    const siblings = link.closest('.header__nav').querySelectorAll("h3")
+    // console.log(link);
+    if (!link.classList.contains("h3")) return;
+   
+    const siblings = link.closest('.header__nav').querySelectorAll("h3");
     siblings.forEach(function(el){
         if (el !== link) el.style.opacity = 0.4;
     })
 })
+    // siblings.forEach(function(el){
+    //     if (el !== link) el.style.opacity = 0.4;
+    // })
 
 nav.addEventListener("mouseout", function(e){
     const link = e.target;
     // console.log(e.target)
+    if (!link.classList.contains("h3")) return;
+   
     const siblings = link.closest('.header__nav').querySelectorAll("h3")
     siblings.forEach(function(el){
         if (el !== link) el.style.opacity = 1;
@@ -134,4 +177,33 @@ thirdBox.forEach(function(box){
 //   nav.addEventListener("mouseout", function(e) {
 //     handleHover(e, 1)
 //   });
+slide.forEach(function(s,i){
+    // s.style.transform = 'scale(0.1)'
+    s.style.transform = `translate(${100*i}%)`
+  });
 
+
+let curSlide = 0;
+const maxSlide = slide.length;
+console.log(maxSlide);
+//cur 0 -> 1 -> 2 -> 3
+//max 1->2->3->4
+  btnRight.addEventListener("click", function(e){
+      e.preventDefault();
+      
+      ////1,2,3,4
+      if (curSlide === maxSlide-1){
+      curSlide = 0;}
+      else{
+        
+        slide.forEach(function(s,i){
+            curSlide++; 
+
+            s.style.transform = `translate(${100*(i - curSlide+)}%)`
+          });
+          //now 0, 100 200, 300
+          // -100, 0, 100, 200
+          //i = 0, 1, 2, 3
+          //c = 1.2.3.4      
+        }
+  });
